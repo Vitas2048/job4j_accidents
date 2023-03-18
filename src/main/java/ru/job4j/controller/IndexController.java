964c7e99a2b6e52dab4c17ae1acb708a8b733910
vitas2048 <vithag97@mail.ru>
2023-03-18 10:25:18 +0700

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/controller/IndexController.java
+++ src/main/java/ru/job4j/controller/IndexController.java
@@ -1,16 +1,24 @@
 package ru.job4j.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.accidents.repository.AccidentHibernate;
 
 @Controller
 public class IndexController {
+    private final AccidentHibernate accidents;
+
+    public IndexController(AccidentHibernate accidents) {
+        this.accidents = accidents;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("users", "Petr Arsentev");
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        model.addAttribute("accidents", accidents.getAll());
         return "index";
     }
 }
